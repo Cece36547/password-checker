aff55ee5d22ace96aadd04975e67b1495445aa82
CierraBlackett <blackettcierra5@gmail.com>
2025-09-18 17:56:01 -0400

    i am pushing latest version

--- pwanalyzer.cpp
+++ pwanalyzer.cpp
@@ -1,5 +1,8 @@
 #include "pwanalyzer.h"
 
+
+Password::Password(): pw(""), spec_char_count(0) {}
+
 void Password::setPw(const std::string &p){
     pw = p;
 }
@@ -7,6 +10,7 @@ void Password::setPw(const std::string &p){
 std::string Password::getPW() const{
     return pw;
 }
+
 
 bool Password::isLengthValid() const {
     if(pw.length() > 10){
@@ -18,25 +22,73 @@ bool Password::isLengthValid() const {
 }
 
 bool Password::hasUppercase() const {
-    return false; //dummy
+    for(int i = 0; i < pw.length(); i++){
+        if(isupper(pw[i])){
+            return true;
+        }
+    }
+    return false;
 }
 
 bool Password::hasLowercase() const {
-    return false; //dummy
+    for(int i = 0; i < pw.length(); i++){
+        if(islower(pw[i])){
+            return true;
+        }
+    }
+    return false;
 }
 
 bool Password::hasDigit() const {
-    return false; //dummy
+    for(int i = 0; i < pw.length(); i++){
+        if(isdigit(pw[i])){
+            return true;
+        }
+    }
+    return false;
 }
 
 bool Password::hasSpecialChar() const {
-    return false; //dummy
+
+    for(int i = 0; i < pw.length(); i++){
+        if(ispunct(pw[i])){
+            return true;
+        }
+    }
+    return false;
 }
 
 bool Password::hasWhitespace() const {
-    return false; //dummy
+        for(int i = 0; i < pw.length(); i++){
+        if(isspace(pw[i])){
+            return true;
+        }
+    }
+    return false;
 }
 
-int Password::strengthScore() const{
+int Password::get_spec_char_count() const{
     return 0;
+}
+
+
+
+//Calculating Strength Score
+int Password::strengthScore(const Password& p) const{
+    int score = 0;
+    
+    //Case 1: If password contains uppercase
+    if (p.hasUppercase()){
+        score += 5;
+    }
+
+    //Case 2: If password contains lowercase
+    if (p.hasLowercase()){
+        score += 5;
+    }
+    //Case 3: If password contains digits
+    if (p.hasDigit()){
+        score += 10;
+    }
+    return score;
 }
--- pwanalyzer.h
+++ pwanalyzer.h
@@ -1,55 +1,75 @@
-#pragma once
+#ifndef PWANALYZER_H
+#define PWANALYZER_H
+
 #include <iostream>
 #include <string>
 class Password{
     private:
-        std::string pw;
+      std::string pw;
+      int spec_char_count;
+
+    //Parameterized Constructor; No "direct" outside creation
+    Password(const std::string& enterered_pw);
+
+    //Helper to calculate spec_char_count
     public:
+    /**
+     * @brief Default Constructor: PW is intialized to an empty string; spec_char_count is intialized to 0
+     */
+    Password();
+
     /**
      * @brief A setter for the password
      * @param p A Constant Reference to a string
      */
-        void setPw(const std::string &p);
+      void setPw(const std::string &p);
 
     /**
      *  @brief A getter for password
      * @return A string variable
      */
 
-        std::string getPW() const;
+      std::string getPW() const;
     /**
      * @brief This method checks if the given password's length is valid. 
      * @return True if length >10. If else, return false.
      */
-        bool isLengthValid() const;
+      bool isLengthValid() const;
 
     /**
      * @brief This method checks is the given password contains at least one uppercase alphabetic character.
      */
-        bool hasUppercase() const;
+      bool hasUppercase() const;
     /**
      * @brief This method checks is the given password contains at least one uppercase alphabetic charater.
      */
-        bool hasLowercase() const;
+      bool hasLowercase() const;
     
     /**
      * @brief This method checks if the given password contains at least one digit.
      */
-        bool hasDigit() const;
+      bool hasDigit() const;
 
      /**
      * @brief This method checks if the given password contains at least one special character.
      */
-        bool hasSpecialChar() const;
+      bool hasSpecialChar() const;
     
      /**
      * @brief This method checks if the given password contains white space.
      */
-        bool hasWhitespace() const;
+      bool hasWhitespace() const;
 
     /**
      * @brief This method will return a score calculating the strength of the password
+     * @param Constant Reference to a Password Object
      */
-        int strengthScore() const;
+      int strengthScore(const Password& p) const;
+
+   /**
+    * @brief Getter for Special Character Count
+    */
+      int get_spec_char_count() const;
 };
 
+#endif // PWANALYZER_H
